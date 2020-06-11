b3927bf0b38d5dc4789836a40220ac141206e854
Azzy <azzy049@gmail.com>
2020-06-11 10:43:56 +0530

    Created new repository for Flask Boiler plate tempate

--- app.py
+++ app.py
@@ -19,18 +19,7 @@ except ImportError:
     print('  json : library not found.  ')
     print('flask : Module not found')
 
-try:
-    import requests
-except ImportError:
-    print('  requests : library not found.  ')
 
-try:
-    import urllib
-except ImportError:
-    print(' urllib : library not found.  ')
-
-sys.path.append('./helpers')
-from helpers.Trace_Parser_7 import *
 
 data = []
 app = Flask(__name__)
@@ -39,17 +28,8 @@ app.secret_key = 'Let it be a secrete'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    #with open('sample.json') as json_file:
-    #    data = json.load(json_file)
-    #result =  execution()
-    result = True
-    if result:
-        file  = open('./helpers/sample.json','r')
-        data = json.load(file)
-        file.close()
-    else:
-        data = None
-    return render_template('index.html', data=data)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
